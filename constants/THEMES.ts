92718c8b652cc1504be51ad61dfbf3e71016c5c0
Cheikh-Kanteye <cheikhkanteye.contact@gmail.com>
2023-08-06 16:17:50 +0000

    Theming

---
+++ constants/THEMES.ts
@@ -0,0 +1,18 @@
+const THEMES = {
+  dark: {
+    primary: "#6dce58",
+    background: "#2a2a2a",
+    btn: "#414141",
+    largeBtn: "#171717",
+    text: "#ffffff",
+  },
+  light: {
+    primary: "#6dcf58",
+    background: "#ffffff",
+    btn: "#eaeaea",
+    largeBtn: "#bababa",
+    text: "#2a2a2a",
+  },
+};
+
+export default THEMES;
